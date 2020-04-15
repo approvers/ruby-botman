fb9bdd98008ccbab53dc85450ed67dcd5a2164fe
Colk-tech <colk.tech@gmail.com>
2020-04-15 09:29:44 +0900

    とりあえずPythonをdisるようにした

--- main.rb
+++ main.rb
@@ -1,12 +1,25 @@
 require 'discordrb'
+require_relative 'lib/kokusei_chosa.rb'
 
 bot = Discordrb::Commands::CommandBot.new(
     token: ENV['TOKEN'],
     prefix:'!',
 )
 
+bot.ready do |event|
+  channel = bot.channel(id=695618239757025290)
+  channel.send_message("R   u   b   y   か   ら   こ   ん   に   ち   は")
+end
+
 bot.command :hello do |event|
   event.send_message("R   u   b   y   か   ら   こ   ん   に   ち   は")
 end
 
-bot.run
+bot.message do |event|
+  if event.content.include?("Python")
+    event.respond("Pythonはクソ！")
+    event.respond("Pythonなんてやめちまえ！w")
+  end
+end
+
+bot.run()
